b0d954ad24c5b668a35017961a43fe7064bcb7ff
Badarokey <badarog7@gmail.com>
2016-11-29 11:01:56 -0200

    Update Perfil2.java

--- app/src/main/java/com/example/gabriel/thediarista/Perfil2.java
+++ app/src/main/java/com/example/gabriel/thediarista/Perfil2.java
@@ -1,7 +1,9 @@
 package com.example.gabriel.thediarista;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Perfil2 extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class Perfil2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil2);
+    }
+
+
+
+    public void VoltaPerfil(View view) {
+        startActivity(new Intent(this, Perfil.class));
+
     }
 }
